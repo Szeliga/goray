c8b38d894a170c06e906a56de9ff9abfd3a910e7
Szymon Szeliga <szymon.szeliga@netguru.co>
2016-08-16 19:14:07 +0200

    Fix import in main.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/szeliga/goray/math"
+	"github.com/szeliga/goray/core"
 )
 
 func main() {
-	fmt.Println(math.Vector{X: 1., Y: 1., Z: 1.})
+	fmt.Println(core.Vector{X: 1., Y: 1., Z: 1.})
 }
